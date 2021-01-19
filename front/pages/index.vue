318125aaeaa508a2ff577c6a5dd1b9a08f89c102
aki158 <hideponpon1931@gmail.com>
2021-01-19 15:16:22 +0900

    [fix]不要ファイル削除

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,33 +1,19 @@
 <template>
-  <div>
-    <button
-      type="button"
-      name="button"
-      @click="getMsg"
-    >
-      RailsからAPIを取得する
-    </button>
-    <div
-      v-for="(msg, i) in msgs"
-      :key="i"
-    >
-      {{ msg }}
-    </div>
-  </div>
+  <v-app>
+    hello.vue
+  </v-app>
 </template>
 
 <script>
 export default {
   data () {
-    return {
-      msgs: []
-    }
+
   },
-  methods: {
-    getMsg () {
-      this.$axios.$get('/api/v1/hello')
-        .then(res => this.msgs.push(res))
-    }
-  }
+  // methods: {
+  //   getMsg () {
+  //     this.$axios.$get('/api/v1/hello')
+  //       .then(res => this.msgs.push(res))
+  //   }
+  // }
 }
 </script>
