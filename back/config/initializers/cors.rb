9ef77c5eba2ff82a375e4cc20655473b7fbe4033
aki158 <hideponpon1931@gmail.com>
2021-01-22 14:10:37 +0900

    [add]rubocopインストール後適用

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["API_DOMAIN"] || ""
+    origins ENV['API_DOMAIN'] || ''
 
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
