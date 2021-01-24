eec3f1e2f8419529bf9a729be0c7d6d54e57dd5d
aki158 <hideponpon1931@gmail.com>
2021-01-24 11:25:09 +0900

    [add]devise/devise_token_authインストール#5

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
