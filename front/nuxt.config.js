38d84d02d62d0109090829d74458e991f5af0113
aki158 <hideponpon1931@gmail.com>
2021-01-24 21:10:31 +0900

    [fix]nuxt.config.js endpoint修正#5

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -100,13 +100,13 @@ export default {
         login: '/users/login',
         logout: '/',
         callback: false,
-        home: '/users/profile',
+        home: '/',
     },
     strategies: {
       local: {
         endpoints: {
-          login: { url: '/api/v1/auth/login', method: 'post', propertyName: 'token' },
-          logout: { url: '/api/v1/auth/logout', method: 'post' },
+          login: { url: '/api/v1/auth/sign_in', method: 'post', propertyName: 'token' },
+          logout: { url: '/api/v1/auth/sign_out', method: 'post' },
           user: false,
         },
       }
