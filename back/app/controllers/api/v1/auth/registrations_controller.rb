41e1e7942a8bb091f58566e7126cc489d78de5bb
aki158 <hideponpon1931@gmail.com>
2021-02-10 13:38:00 +0900

    [fix]rubocop指摘修正

--- back/app/controllers/api/v1/auth/registrations_controller.rb
+++ back/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,13 +1,18 @@
-class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  
-  private
+module Api
+  module V1
+    module Auth
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+        private
 
-  # ストロングパラメーター設定
-  def sign_up_params
-    params.permit(:name, :email, :password, :password_confirmation, :image)
-  end
+        # ストロングパラメーター設定
+        def sign_up_params
+          params.permit(:name, :email, :password, :password_confirmation, :image)
+        end
 
-  def account_update_params
-    params.permit(:name, :email, :image)
+        def account_update_params
+          params.permit(:name, :email, :image)
+        end
+      end
+    end
   end
 end
--- back/app/controllers/api/v1/users_controller.rb
+++ back/app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,21 @@
-class Api::V1::UsersController < ApplicationController
-  def index
-    @user = User.all
-    render json: @user, status: :ok
-  end
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        @user = User.all
+        render json: @user, status: :ok
+      end
 
-  def show
-    @user = User.find(params[:id])
-    render json: @user, status: :ok
-  end
+      def show
+        @user = User.find(params[:id])
+        render json: @user, status: :ok
+      end
 
-  private
+      private
 
-  def food_params
-    params.permit(:name, :details, :calorie, :carbonhydrate, :protein, :lipid, :category, :maker, :image)
+      def food_params
+        params.permit(:name, :details, :calorie, :carbonhydrate, :protein, :lipid, :category, :maker, :image)
+      end
+    end
   end
 end
