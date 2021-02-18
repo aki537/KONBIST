9059ddd658812260420cbf56bc8e95ca5103c566
aki158 <hideponpon1931@gmail.com>
2021-02-18 13:18:24 +0900

    [create]follow用テーブル作成#9

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       resources :foods
       resources :users, only: [:index, :show]
       resources :reviews, only: [:create, :update, :destroy]
+      resources :relationships, only: [:create, :destroy]
       resource :food_likes, only: [:create, :destroy]
       resource :review_likes, only: [:create, :destroy]
     end
