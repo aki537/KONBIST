44d661be0d98dde1ee25a3449d94afb01aa964bb
aki158 <hideponpon1931@gmail.com>
2021-03-06 22:29:11 +0900

    [fix]favicon,メタtitle変更

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -6,8 +6,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: "%s - " + process.env.npm_package_name,
-    title: process.env.npm_package_name || "",
+    title: "KONBIST",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
